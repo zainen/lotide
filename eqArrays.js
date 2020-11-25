be911fba1fd0f71bf8e35d80f95791588f5fd5dd
zainen <zainensuzuki@gmail.com>
2020-11-25 00:58:21 +0000

    with after eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,15 @@
 const eqArrays = function(actual, expected) {
-  let isEqual = true
-  // for loop starting i = 0 until i < actual/expected.length i++
-  // if actual[i] === expected[i]
-  // return true
+  let isEqual = true;
   for (let i = 0; i < expected.length; i++) {
     if (actual[i] !== expected[i]) {
-      isEqual = false
+      isEqual = false;
     }
   }
-  
-  // if (actual === expected) {
-  //   console.log('hi')
-  //   return true
-  // } else {
-  //   return false
-  // }
-  return isEqual
+  return isEqual;
 };
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // =
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // =
