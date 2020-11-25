6f8d1b4e2a83087f91403ae467986c0f11ecf390
zainen <zainensuzuki@gmail.com>
2020-11-25 01:12:51 +0000

    finished

---
+++ assertArrayEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(actual, expected) {
+  let isEqual = false;
+  for (let i = 0; i < expected.length; i++) {
+    if (actual[i] !== expected[i]) {
+      isEqual = false;
+    } else {
+      isEqual = true;
+    }
+  }
+  return isEqual;
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+
+console.log(assertEqual(eqArrays("Lighthouse Labs", "Lighthouse Labs"), true));
+console.log(assertEqual(eqArrays(1, 2), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true));// => false
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,15 @@
 const eqArrays = function(actual, expected) {
-  let isEqual = true;
+  let isEqual = false;
   for (let i = 0; i < expected.length; i++) {
     if (actual[i] !== expected[i]) {
       isEqual = false;
+    } else {
+      isEqual = true;
     }
   }
   return isEqual;
 };
+
 
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
