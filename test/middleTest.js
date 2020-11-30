19d9cc45d71d7ae0f61936a137456e1f9e2cea18
zainen <zainensuzuki@gmail.com>
2020-11-30 22:54:42 +0000

    modded with tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,32 @@
+const assert = require('chai').assert
 const middle = require('../middle')
 const assertArrayEqual = require('../assertArrayEqual')
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-console.log(assertArrayEqual(middle([2]), [], true))
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2])) // => []
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+// console.log(assertArrayEqual(middle([2]), []))
+// console.log(assertArrayEqual(middle([1,2,3]), 2))
+// console.log(assertArrayEqual(middle([1,2,3,4]), [2,3]))
+
+
+describe('#middle', () => {
+  it('return middle length < 3 = []', () => {
+    const arr = [1,2]
+    console.log(middle(arr))
+    assert.isTrue(middle(arr).length === 0)
+  })
+  it('return middle length odd > 3 ', () => {
+    const arr = [1,2,3]
+    console.log(middle(arr))
+    assert.isTrue(middle(arr) === 2)
+  })
+  it('return middle 2, 3 if length even > 2', () => {
+    const arr = [1,2,3,4]
+    console.log(middle(arr))
+    assertArrayEqual(middle(arr), [2, 3])
+  })
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,22 @@
+const assert = require('chai').assert
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]).length;
-console.log(assertEqual(result, ["Lighthouse", "Labs"].length)); // => will always fail!
+// const result = tail(["Hello", "Lighthouse", "Labs"]).length;
+// console.log(assertEqual(result, ["Lighthouse", "Labs"].length)); // => will always fail!
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
+
+describe('#tail', () => {
+  it('return length of array equal', () => {
+    const words = ['hello', 'lighthouse', 'labs']
+    assert.strictEqual(tail(words).length, 2)
+  })
+  it('return length of original arrray unchanged', () => {
+    const words = ['hello', 'lighthouse', 'labs']
+    assert.strictEqual(words.length, 3)
+
+  })
+}) 
