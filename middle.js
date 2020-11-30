480e040074fe9fac74cfcc0b521bb737f8a71baa
zainen <zainensuzuki@gmail.com>
2020-11-30 23:05:56 +0000

    mimic lodash git push

--- middle.js
+++ middle.js
@@ -19,12 +19,12 @@ const middle = function(arr) {
 }
 
 module.exports = middle
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-console.log(assertArrayEqual(middle([2]), []))
-console.log(assertArrayEqual(middle([1,2,3]), 2))
-console.log(assertArrayEqual(middle([1,2,3,4]), [2,3]))
+// console.log(middle([1])) // => []
+// console.log(middle([1, 2])) // => []
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+// console.log(assertArrayEqual(middle([2]), []))
+// console.log(assertArrayEqual(middle([1,2,3]), 2))
+// console.log(assertArrayEqual(middle([1,2,3,4]), [2,3]))
